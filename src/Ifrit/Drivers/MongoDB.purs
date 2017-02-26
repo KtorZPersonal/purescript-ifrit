b07477f19bd4345c7b6db17601415663f9dbeb98
KtorZ <matthias.benkort@gmail.com>
2017-02-26 15:41:19 +0100

    get rid of ADT 'Expression' -> use of Json directly (same meaning)

--- src/Ifrit/Drivers/MongoDB.purs
+++ src/Ifrit/Drivers/MongoDB.purs
@@ -1,15 +1,12 @@
 module Ifrit.Drivers.MongoDB
   ( class Ingest
-  , Expression(..)
   , ingest
   ) where
 
 import Prelude
-import Data.Argonaut.Core(Json, stringify)
-import Data.Argonaut.Encode(class EncodeJson, encodeJson)
-import Data.StrMap(StrMap)
+import Data.Argonaut.Core(Json)
+import Data.Argonaut.Encode(encodeJson, (:=))
 import Data.StrMap(fromFoldable) as M
-import Data.List(List)
 import Data.List(fromFoldable) as L
 import Data.Tuple(Tuple(..))
 import Ifrit.Core(Stage(..), Reduce(..), Map(..), Terminal(..))
@@ -18,59 +15,35 @@ import Ifrit.Core(Stage(..), Reduce(..), Map(..), Terminal(..))
 -- CLASSES
 
 class Ingest stage where
-  ingest :: stage -> Expression
-
-
--- TYPES
-
-data Expression
-  = ValueString String
-  | ValueBoolean Boolean
-  | ValueNumber Number
-  | Object (StrMap Expression)
-  | List (List Expression)
-
-
-str :: String -> Expression
-str s = ValueString s
-
-
-bool :: Boolean -> Expression
-bool b = ValueBoolean b
-
-
-number :: Number -> Expression
-number n = ValueNumber n
-
-
-object :: Array (Tuple String Expression) -> Expression
-object xs = Object $ M.fromFoldable xs
-
-
-singleton :: String -> Expression -> Expression
-singleton k x = Object $ M.fromFoldable [Tuple k x]
-
-
-list :: Array Expression -> Expression
-list xs = List $ L.fromFoldable xs
-
-infix 7 Tuple as :-
+  ingest :: stage -> Json
 
 
 -- UTILITIES
 
-selector :: Terminal -> Expression
-selector (Field f) = str ("$$this." <> f)
-selector (ConstantString c) = str c
-selector (ConstantBoolean c) = bool c
-selector (ConstantNumber c) = number c
+object :: Array (Tuple String Json) -> Json
+object = M.fromFoldable >>> encodeJson
+
+
+singleton :: String -> Json -> Json
+singleton k x = object [Tuple k x]
+
+
+list :: Array Json -> Json
+list = L.fromFoldable >>> encodeJson
+
+
+selector :: Terminal -> Json
+selector (Field f) = encodeJson ("$$this." <> f)
+selector (ConstantString c) = encodeJson c
+selector (ConstantBoolean c) = encodeJson c
+selector (ConstantNumber c) = encodeJson c
 
 
 -- INSTANCES :: ingest
 
 instance ingestStage :: Ingest Stage where
   ingest (Map m) =
-    singleton "$project" $ Object (map ingest m)
+    singleton "$project" $ encodeJson (map ingest m)
 
 
 instance ingestMapOperator :: Ingest Map where
@@ -80,13 +53,13 @@ instance ingestMapOperator :: Ingest Map where
     let
       src' = ingest src
       sum = list
-        [ str "$$value"
+        [ encodeJson "$$value"
         , selector target
         ]
       reduce = object
-        [ "input" :- src'
-        , "initialValue" :- (number 0.0)
-        , "in" :- (singleton "$add" sum)
+        [ "input" := src'
+        , "initialValue" := (encodeJson 0.0)
+        , "in" := (singleton "$add" sum)
         ]
       divide = list
         [ singleton "$reduce" reduce
@@ -103,36 +76,15 @@ instance ingestReduce :: Ingest Reduce where
 
 instance ingestTerminal :: Ingest Terminal where
   ingest (Field f) =
-    ValueString $ "$" <> f
+    encodeJson $ "$" <> f
   ingest (ConstantString c) =
-    ValueString c
+    encodeJson  c
   ingest (ConstantBoolean c) =
-    ValueBoolean c
+    encodeJson c
   ingest (ConstantNumber c) =
-    ValueNumber c
+    encodeJson c
 
 
 instance ingestArray :: Ingest a => Ingest (Array a) where
   ingest =
-    L.fromFoldable >>> map ingest >>> List
-
--- INSTANCE :: ENCODEJSON
-
-instance encodeJsonExpression :: (EncodeJson String, EncodeJson Int, EncodeJson (StrMap Json), EncodeJson (List Json))
-  => EncodeJson Expression where
-  encodeJson (ValueString s) =
-    encodeJson s
-  encodeJson (ValueBoolean b) =
-    encodeJson b
-  encodeJson (ValueNumber n) =
-    encodeJson n
-  encodeJson (Object m) =
-    encodeJson $ encodeJson <$> m
-  encodeJson (List xs) =
-    encodeJson $ encodeJson <$> xs
-
-
--- INSTANCE :: SHOW
-
-instance showExpression :: EncodeJson Expression => Show Expression where
-  show = encodeJson >>> stringify
+    L.fromFoldable >>> map ingest >>> encodeJson
