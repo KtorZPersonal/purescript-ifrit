3b95ee7b26759ac216f3aefc240f2aab18cda7ae
KtorZ <matthias.benkort@gmail.com>
2017-03-04 20:48:48 +0100

    rework test suite to read all fixtures from a folder

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,88 +1,115 @@
+
 module Test.Main where
 
 import Prelude
 
 import Control.Monad.Aff(Aff)
+import Control.Monad.Aff.AVar(AVAR)
+import Control.Monad.Eff(Eff)
 import Control.Monad.Eff.Class(liftEff)
+import Control.Monad.Eff.Console(CONSOLE)
 import Control.Monad.Eff.Exception(EXCEPTION)
+import Control.Monad.State(runStateT)
 import Data.Argonaut.Core(Json, toObject)
-import Data.Argonaut.Decode(decodeJson, (.?))
+import Data.Argonaut.Decode(decodeJson)
+import Data.Argonaut.Decode.Combinators((.?), (.??))
 import Data.Argonaut.Encode(encodeJson)
 import Data.Argonaut.Parser(jsonParser)
+import Data.Array(foldl)
 import Data.Either(Either(..))
+import Data.Filterable(filterMap)
 import Data.Maybe(Maybe(..))
-import Data.Pair(Pair(..))
 import Data.Traversable(traverse)
+import Data.Tuple(Tuple(..))
 import Ifrit.Core(Pipeline, JsonSchema)
-import Ifrit.Drivers.MongoDB(ingest, ingestType)
+import Ifrit.Drivers.MongoDB(ingest)
 import Node.Encoding (Encoding(..))
 import Node.FS(FS)
-import Node.Path(concat)
-import Node.Globals(__dirname)
 import Node.FS.Sync as FS
-import Test.Unit (Test, TestSuite, suite, test, failure)
+import Node.Globals(__dirname)
+import Node.Path(concat, extname)
+import Test.Unit (Test, test, failure, success)
 import Test.Unit.Assert as Assert
+import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 
-load :: forall eff.
-  String
-  -> Aff (fs :: FS, err :: EXCEPTION | eff) (Either String (Pair Json))
-load fixture =
-  let
-    files = Pair <$> FS.readTextFile UTF8 (concat [__dirname, "..", "..", "test", "fixtures", fixture, "in.json"])
-                 <*> FS.readTextFile UTF8 (concat [__dirname, "..", "..", "test", "fixtures", fixture, "out.json"])
-  in
-    liftEff (traverse jsonParser <$> files)
+testDir :: String
+testDir =
+  concat [__dirname, "..", "..", "test", "fixtures"]
 
-run :: forall e. Either String (Pair Json) -> Test e
-run (Left e) =
-  failure e
-run (Right (Pair input output)) =
-  let
-    sIn :: Maybe (Either String JsonSchema)
-    sIn = (\obj -> do
-      schema <- obj .? "schema"
-      decodeJson schema
-    ) <$> toObject input
+maybeSpec :: String -> String -> Maybe String
+maybeSpec base file =
+  if extname file == ".json"
+  then Just (concat [base, file])
+  else Nothing
 
-    sOut :: Maybe (Either String Json)
-    sOut = (\obj -> obj .? "schema") <$> toObject output
+loadFixtures :: forall e. String -> Aff (fs :: FS, err :: EXCEPTION | e) (Either String (Array Json))
+loadFixtures dir = liftEff do
+  files <- FS.readdir dir
+  values <- traverse (FS.readTextFile UTF8) (filterMap (maybeSpec dir) files)
+  pure $ traverse jsonParser values
 
-    pIn :: Maybe (Either String Pipeline)
-    pIn = (\obj -> do
-      schema <- obj .? "pipeline"
-      decodeJson schema
-    ) <$> toObject input
 
-    pOut :: Maybe (Either String Json)
-    pOut = (\obj -> obj .? "pipeline") <$> toObject output
+fromFixture :: forall e. Json -> Either String (Test (fs :: FS, err :: EXCEPTION | e))
+fromFixture json =
+  case toObject json of
+    Nothing ->
+      Left "invalid provided fixture: not a valid JSON object"
 
-    assertSchema :: JsonSchema -> Pipeline -> Json -> Test e
-    assertSchema sIn pIn sOut =
-      case ingestType sIn pIn of
-      Left err ->
-        failure err
-      Right sOut' ->
-        Assert.equal sOut $ (encodeJson sOut')
-  in
-
-    case { sIn, sOut, pIn, pOut } of
-    { sIn: Just (Right sIn), sOut: Just (Right sOut), pIn: Just (Right pIn), pOut: Just (Right pOut) } ->
+    Just obj ->
       do
-        assertSchema sIn pIn sOut
-        Assert.equal pOut $ (encodeJson $ ingest pIn)
-    _ ->
-      failure "invalid provided fixture: expecting an object with 'schema' and 'pipeline' properties"
+        (schema_in :: JsonSchema) <- obj .? "schema_in" >>= decodeJson
+        (pipeline_in :: Pipeline) <- obj .? "pipeline_in" >>= decodeJson
+        (error :: Maybe String) <- obj .?? "error"
+        (pipeline_out :: Maybe Json) <- obj .?? "pipeline_out"
+        (schema_out :: Maybe Json) <- obj .?? "schema_out"
+
+        case Tuple error [schema_out, pipeline_out] of
+          --
+          -- Error Scenarios
+          --
+          Tuple (Just error') [Nothing, Nothing] ->
+            case runStateT (ingest pipeline_in) schema_in of
+              Right _ ->
+                Left "expected ingest to fail but was successful"
+              Left error'' ->
+                pure $ Assert.equal error' error''
+
+          --
+          -- Successful Scenarios
+          --
+          Tuple Nothing [Just schema_out', Just pipeline_out'] ->
+            case runStateT (ingest pipeline_in) schema_in of
+              Right (Tuple pipeline_out'' schema_out'') ->
+                pure $ do
+                  Assert.equal pipeline_out' pipeline_out''
+                  Assert.equal schema_out' (encodeJson schema_out'')
+              Left err ->
+                Left err
+
+          --
+          -- Weirdly Formatted Fixtures
+          --
+          _ ->
+           Left "invalid provided fixture: properties messed up"
 
 
-step :: forall eff. String -> TestSuite (fs :: FS, err :: EXCEPTION | eff)
-step fixture = test fixture do
-  scenario <- load fixture
-  run scenario
-
-
-main = runTest do
-  suite "stage :: map" do
-    step "single-field-project"
-    step "single-constant-project"
-    step "avg-nested-array"
+main :: Eff
+  ( fs :: FS
+  , err :: EXCEPTION
+  , console :: CONSOLE
+  , testOutput :: TESTOUTPUT
+  , avar :: AVAR
+  ) Unit
+main =
+  let
+    mongodbSuite = test "mongodb" do
+      fixtures <- loadFixtures (concat [testDir, "mongodb"])
+      let tests = fixtures >>= traverse fromFixture
+      case tests of
+        Left err ->
+          failure err
+        Right tests' ->
+          foldl (\p s -> p *> s) success tests'
+  in
+    runTest mongodbSuite
