91e565468fa0cca2cd4b092126628bd0ed0297a0
KtorZ <matthias.benkort@gmail.com>
2017-03-28 00:36:42 +0200

    add Core file with compiler functions

---
+++ src/Ifrit/Core.purs
@@ -0,0 +1,28 @@
+module Ifrit.Core where
+
+import Prelude
+
+import Control.Monad.State(evalStateT)
+import Data.Argonaut.Core(Json)
+import Data.Argonaut.Encode(encodeJson)
+import Data.Either(Either)
+
+import Ifrit.Lexer as Lexer
+import Ifrit.Parser as Parser
+import Ifrit.Semantic as Semantic
+import Ifrit.Driver.MongoDB as MongoDB
+
+class Compile driver where
+  compile :: Json -> String -> Either String { schema :: Json, output :: driver }
+
+newtype MongoDB = MongoDB Json
+
+
+instance compileMongoDB :: Compile MongoDB where
+  compile inputSchema input = do
+    schemaIn <- Semantic.fromJson inputSchema
+    tokens <- evalStateT Lexer.tokenize { pos: 0, str: input }
+    ast :: Parser.Statement <- evalStateT Parser.parse tokens
+    schema <- Semantic.analyze schemaIn ast
+    output <- MongoDB.ingest ast
+    pure { schema: encodeJson schema, output: MongoDB output }
